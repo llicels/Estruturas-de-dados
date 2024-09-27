9c363e07026479af095239904f08f91dd36b05df
Alice <alicelsdev@gmail.com>
2024-09-27 11:48:21 -0300

    corrigir arvoreBin.c

---
+++ arvores/arvoreBin.c
@@ -0,0 +1,48 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+typedef struct{
+    char chave;
+} REGISTRO;
+
+typedef struct no_{
+    REGISTRO reg;
+    struct NO_ *esq;
+    struct NO_ *dir;
+} NO;
+
+typedef NO ARVORE;
+
+void minha_funcao(REGISTRO *reg){
+    printf("%c ", reg->chave);
+}
+
+void pre_ordem(ARVORE *arvore, void (*func)(REGISTRO*)){
+    if(arvore == NULL) return;
+    func(&arvore->reg);
+    pre_ordem(arvore->esq, func);
+    pre_ordem(arvore->dir, func);
+}
+
+int main()
+{
+    ARVORE a;
+    NO *no;
+
+    a.reg.chave = 'A';
+
+    no = malloc(sizeof(NO));
+    no->reg.chave = 'B';
+    no->esq = NULL;
+    no->dir = NULL;
+    a.esq = no;
+
+    no = malloc(sizeof(NO));
+    no->reg.chave = 'C';
+    no->esq = NULL;
+    no->dir = NULL;
+    a.dir = no;
+
+    pre_ordem(&a, &minha_funcao);
+    return 0;
+}
